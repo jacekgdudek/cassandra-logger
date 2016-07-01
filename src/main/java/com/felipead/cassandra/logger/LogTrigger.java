e86098c252dd94b09d035b9e31a825728d058436
Jacek Dudek <jacek.dudek@urbantide.com>
2016-07-01 14:13:48 +0100

    default localhost port changed

--- src/main/java/com/felipead/cassandra/logger/LogTrigger.java
+++ src/main/java/com/felipead/cassandra/logger/LogTrigger.java
@@ -18,7 +18,7 @@ import java.util.Collections;
 
 @SuppressWarnings("UnusedDeclaration")
 public class LogTrigger implements ITrigger {
-    public static final String LOCALHOST = "10.0.2.197";
+    public static final String LOCALHOST = "127.0.0.1:9177";
     
     private static final Logger logger = LoggerFactory.getLogger(LogTrigger.class);
 
